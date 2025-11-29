cabe3401dd7ce3d24fe0daa541ac71e3f12a817e
Ruben Aruzamen <Saelthas@gmail.com>
2025-11-28 20:28:07 -0400

    addeed to master

--- 10-practica/variables.tf
+++ 10-practica/variables.tf
@@ -1,13 +1,14 @@
-variable "common_tags" {
-  description = "Common Tags"
-  type        = map(string)
-}
+# variable "common_tags" {
+#   description = "Common Tags"
+#   type        = string
+# }
 variable "vnet_name" {
   description = "vnet name"
   type        = string
+  default = "rAruzamen-dev-net"
 }
 variable "addres_space" {
   description = "Segmento principal de la vnet"
   type        = list(string)
-
+  default     = ["10.0.0.0/16"]
 }
